5615c5f28d8d44aae966831fdbd9ee43268ce7f7
David <david@MacBook-Pro-Mobile-David.local>
2020-04-30 19:02:11 -0500

    add default values in buildSrc

---
+++ buildSrc/src/main/java/Config.kt
@@ -0,0 +1,21 @@
+object DefaultConfig {
+    const val compileSdkVersion = 29
+    const val buildToolsVersion = "29.0.3"
+    const val minSdkVersion = 21
+    const val targetSdkVersion = 29
+}
+
+object Network {
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object Stats {
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object App {
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
